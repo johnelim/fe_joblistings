aa6c49d3f7c82007bdcee26c705aa3e6ffba187e
john-lim <john.lim@exactusenergy.com>
2025-05-30 03:40:32 +1000

    fixed navbar background

--- joblistings/src/components/Header.tsx
+++ joblistings/src/components/Header.tsx
@@ -2,7 +2,7 @@ import FilterBar from "./FilterBar";
 
 export default function Header() {
   return (
-    <header className="h-24 relative bg-cover bg-[url('./src/assets/bg-header-mobile.svg')] sm:bg-[url('./src/assets/bg-header-desktop.svg')]">
+    <header className="h-24 relative bg-cover bg-[url('/assets/bg-header-mobile.svg')] sm:bg-[url('/assets/bg-header-desktop.svg')]">
       <FilterBar />
     </header>
   );
